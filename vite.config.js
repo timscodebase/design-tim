dfa6ede79c18bb3db70b05d91154486715891e27
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-11-30 18:48:02 -0800

    Update dependencies in package.json and pnpm-lock.yaml

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-const svg = !!process.env.USE_SVG
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		svelte({
-			preprocess: svg ? [vitePreprocess({ svgo: true, useSimpleStringParser: true })] : [],
-			experimental: {
-				prebundleSvelteLibraries: true
-			}
-		})
-	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
