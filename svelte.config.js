18e38a815447842d8715f1779be6715edab7406f
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-12-10 20:52:54 -0800

    Add outDir to svelte.config.js and update tsconfig.json

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,7 @@ const mdsvexOptions = {
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	adapter: adapter(),
+	outDir: 'build',
 	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), preprocess(), mdsvex(mdsvexOptions)],
 	kit: {
