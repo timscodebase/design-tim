de92dce29ae61696a9119147e9859738206da755
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-12-17 15:49:57 -0800

    Add external link to project thumbnail

--- src/lib/components/Project/Project.svelte
+++ src/lib/components/Project/Project.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
+	import { Link } from '$lib'
 	export let project
 
 	const { name, thumbnail, image, description, medium } = project
 </script>
 
 <article class="project">
-	<img src={thumbnail} />
+	<Link external={true}>
+		<img src={thumbnail} />
+	</Link>
 	<h2>{name}</h2>
 	<p>{description}</p>
 	<p>{medium}</p>
-	<img src={image} alt={name} />
 </article>
 
 <style>
