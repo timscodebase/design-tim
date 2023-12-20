1a18a71fe8df3185037871e26e0ab3aa545a3e06
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-12-20 09:20:07 -0800

    Update grid-template-rows and align-items in Item.svelte

--- src/lib/components/BucketList/Item.svelte
+++ src/lib/components/BucketList/Item.svelte
@@ -9,14 +9,16 @@
 	<p>{description}</p>
 	{#if image}
 		<Link href={image}><img src={thumb} alt={goal} /></Link>
+	{:else}
+		<div />
 	{/if}
 </div>
 
 <style>
 	div {
 		display: grid;
-		grid-template-rows: 1fr auto auto;
-		align-content: baseline;
+		grid-template-rows: 50px auto 1fr;
+		align-items: start;
 		gap: var(--gap);
 	}
 
