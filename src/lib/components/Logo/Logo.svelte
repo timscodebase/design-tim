afb1fade857cbd531a858c9270ced3e7c4cd10f7
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-01 19:01:33 -0800

    Fix bug in login functionality

--- src/lib/components/Logo/Logo.svelte
+++ src/lib/components/Logo/Logo.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { name, description } from '$lib/config'
 	const logo =
-		'https://res.cloudinary.com/tithos/image/upload/f_auto,q_auto/v1703998982/design-tim-genearal-banner_y3aqyx.png'
+		'https://res.cloudinary.com/tithos/image/upload/c_scale,f_auto,q_auto:eco,w_446/v1703998982/design-tim-genearal-banner_y3aqyx.png'
 </script>
 
 <a href="/">
