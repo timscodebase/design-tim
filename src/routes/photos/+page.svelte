8c2b52bdfdb38c9420450341bfd85fd03320b901
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-08 19:26:52 -0800

    Update photo gallery layout based on screen width

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,18 +1,37 @@
 <script lang='ts'>
+  import { onMount } from 'svelte';
   import ImageGallery from '@react2svelte/image-gallery';
 
 	export let data: PageData
 	const { photos } = data
+
+  let width = 0
+
+  onMount(() => {
+    width = window.innerWidth
+  })
 </script>
 
 <h2>Photos</h2>
 
-<ImageGallery
-  items={photos}
-  lazyLoad={true}
-  showPlayButton={false}
-  showBullets={true}
-/>
+{#if width > 500}
+  <ImageGallery
+    items={photos}
+    lazyLoad={true}
+    showPlayButton={false}
+    showBullets={true}
+    showThumbnails={false}
+  />
+{:else}
+  <ImageGallery
+    items={photos}
+    lazyLoad={true}
+    showPlayButton={false}
+    showBullets={true}
+    showThumbnails={false}
+    showNav={false}
+  />
+{/if}
 
 <style>
   
