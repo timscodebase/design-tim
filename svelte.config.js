375426f62424f3e2e7172dc35dc307efab45ab18
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-15 13:49:16 -0800

    code formating

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,24 @@ import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
+import highlighter from './src/lib/utils/codeHighlighter.mjs';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md', '.svelte', '.svx']
+	extensions: ['.md', '.svelte', '.svx'],
+	highlight: {
+    highlighter,
+  },
 }
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), preprocess(), mdsvex(mdsvexOptions)],
+	preprocess: [
+		vitePreprocess(),
+		preprocess(),
+		mdsvex(mdsvexOptions)
+	],
 	kit: {
 		adapter: adapter(),
 		alias: {
