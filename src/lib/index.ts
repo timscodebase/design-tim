5b0a45f732b258dd0bb42b6d59a17437ee8d1ba7
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-16 20:25:35 -0800

    Commented out Quote component import and export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -61,5 +61,5 @@ export { Tags }
 import Teacher from './components/Teacher'
 export { Teacher }
 
-import Quote from './components/Quote'
-export { Quote }
+// import Quote from './components/Quote'
+// export { Quote }
