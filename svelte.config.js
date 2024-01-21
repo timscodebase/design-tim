3e650db3de87cd1d73653cb5999ea43677d2f8a1
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-21 11:09:44 -0800

    Update Svelte adapter in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-bun'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
