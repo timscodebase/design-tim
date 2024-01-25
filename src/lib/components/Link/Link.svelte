c8ff34244fde5640e61981cf1e07fc3d69d12194
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-25 14:05:36 -0800

    Remove active class styling from Link component

--- src/lib/components/Link/Link.svelte
+++ src/lib/components/Link/Link.svelte
@@ -35,10 +35,10 @@
 		word-break: break-word;
 	}
 
-	.active {
+	/* .active {
 		background: var(--primary-2);
 		color: var(--reverse-text);
 		padding: 3px 5px;
 		view-transition-name: active-page;
-	}
+	} */
 </style>
