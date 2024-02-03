02181a03fa52111abc6996f156bf52bdcc43785b
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-02-03 14:25:52 -0800

    GDPR CPRA

---
+++ src/routes/policies/+page.svelte
@@ -0,0 +1,228 @@
+<script lang="ts">
+	import { name } from '$lib/config'
+</script>
+
+<h2>Policies</h2>
+
+<section id="gdpr">
+	<h3>{name} GDPR Compliance Statement</h3>
+
+	<h4>Introduction</h4>
+	Welcome to {name}! This statement is designed to inform you about our commitment to protecting
+	your privacy and ensuring the security of your personal information in accordance with the General
+	Data Protection Regulation (GDPR).
+
+	<h4>Data Controller</h4>
+	The data controller for {name} is {name}. You can contact us at
+	<a href="mailto:tim@timsmith.tech">tim@timsmith.tech</a>.
+
+	<h4>Information We Collect</h4>
+
+	<h4>1. Personal Information</h4>
+	<p>We may collect and process the following personal information:</p>
+
+	<ul>
+		<li>Name: To personalize your experience.</li>
+		<li>Email Address: To communicate with you and send newsletters (if you subscribe).</li>
+	</ul>
+
+	<h4>2. Non-Personal Information</h4>
+	<p>We may also collect non-personal information such as:</p>
+	<ul>
+		<li>Browser type</li>
+		<li>Device type</li>
+		<li>IP address</li>
+		<li>Cookies data</li>
+	</ul>
+
+	<h4>How We Use Your Information</h4>
+	<p>We use your information for the following purposes:</p>
+
+	<ul>
+		<li>Personalize your experience on the blog.</li>
+		<li>Communicate with you through newsletters (if you subscribe).</li>
+		<li>Analyze and improve our website's performance.</li>
+		<li>Ensure the security of our website.</li>
+	</ul>
+
+	<h4>Legal Basis for Processing</h4>
+	<p>We process personal information based on the following legal bases:</p>
+
+	<ul>
+		<li><strong>Consent:</strong> For newsletter subscriptions.</li>
+		<li><strong>Legitimate Interests:</strong> For improving our website and ensuring security.</li>
+	</ul>
+
+	<h4>Data Sharing</h4>
+	<p>
+		We do not sell, trade, or otherwise transfer your personal information to third parties without
+		your consent. However, we may share your data with trusted third parties who assist us in
+		operating our website or conducting our business.
+	</p>
+
+	<h4>Data Security</h4>
+	<p>
+		We take appropriate measures to ensure the security of your personal information. This includes
+		encryption, secure hosting, and regular security audits.
+	</p>
+	<h4>Your Rights</h4>
+	<p>Under the GDPR, you have the following rights:</p>
+
+	<ul>
+		<li>
+			<strong>Right to Access:</strong> You can request access to the personal information we hold about
+			you.
+		</li>
+		<li>
+			<strong>Right to Rectification:</strong> You can request corrections to your personal information.
+		</li>
+		<li>
+			<strong>Right to Erasure:</strong> You can request the deletion of your personal information.
+		</li>
+		<li>
+			<strong>Right to Object:</strong> You can object to the processing of your personal information.
+		</li>
+	</ul>
+
+	<p>
+		To exercise these rights, please contact us at<a href="mailto:tim@timsmith.tech"
+			>tim@timsmith.tech</a
+		>.
+	</p>
+
+	<h4>Cookie Policy</h4>
+	<p>
+		We use cookies to enhance your experience on our website. By using our website, you consent to
+		our use of cookies.
+	</p>
+
+	<h4>Changes to This Privacy Statement</h4>
+	<p>
+		We may update this privacy statement to reflect changes in our practices or for other
+		operational, legal, or regulatory reasons. Please check this page regularly for updates.
+	</p>
+
+	<h4>Contact Information</h4>
+	<p>
+		If you have any questions or concerns about our GDPR compliance or privacy practices, please
+		contact us at
+		<a href="mailto:tim@timsmith.tech">tim@timsmith.tech</a>. Last Updated: 02/03/2024
+	</p>
+</section>
+
+<section id="cpra">
+	<h3>{name} CPRA Compliance Statement</h3>
+
+	<h4>Introduction</h4>
+	Welcome to {name}! This statement is designed to inform you about our commitment to protecting
+	your privacy and ensuring the security of your personal information in accordance with the General
+	Data Protection Regulation (CPRA).
+
+	<h4>Data Controller</h4>
+	The data controller for {name} is {name}. You can contact us at
+	<a href="mailto:tim@timsmith.tech">tim@timsmith.tech</a>.
+
+	<h4>Information We Collect</h4>
+
+	<h4>1. Personal Information</h4>
+	<p>We may collect and process the following personal information:</p>
+
+	<ul>
+		<li>Name: To personalize your experience.</li>
+		<li>Email Address: To communicate with you and send newsletters (if you subscribe).</li>
+	</ul>
+
+	<h4>2. Non-Personal Information</h4>
+	<p>We may also collect non-personal information such as:</p>
+	<ul>
+		<li>Browser type</li>
+		<li>Device type</li>
+		<li>IP address</li>
+		<li>Cookies data</li>
+	</ul>
+
+	<h4>How We Use Your Information</h4>
+	<p>We use your information for the following purposes:</p>
+
+	<ul>
+		<li>Personalize your experience on the blog.</li>
+		<li>Communicate with you through newsletters (if you subscribe).</li>
+		<li>Analyze and improve our website's performance.</li>
+		<li>Ensure the security of our website.</li>
+	</ul>
+
+	<h4>Legal Basis for Processing</h4>
+	<p>We process personal information based on the following legal bases:</p>
+
+	<ul>
+		<li><strong>Consent:</strong> For newsletter subscriptions.</li>
+		<li><strong>Legitimate Interests:</strong> For improving our website and ensuring security.</li>
+	</ul>
+
+	<h4>Data Sharing</h4>
+	<p>
+		We do not sell, trade, or otherwise transfer your personal information to third parties without
+		your consent. However, we may share your data with trusted third parties who assist us in
+		operating our website or conducting our business.
+	</p>
+
+	<h4>Data Security</h4>
+	<p>
+		We take appropriate measures to ensure the security of your personal information. This includes
+		encryption, secure hosting, and regular security audits.
+	</p>
+	<h4>Your Rights</h4>
+	<p>Under the CPRA, you have the following rights:</p>
+
+	<ul>
+		<li>
+			<strong>Right to Access:</strong> You can request access to the personal information we hold about
+			you.
+		</li>
+		<li>
+			<strong>Right to Rectification:</strong> You can request corrections to your personal information.
+		</li>
+		<li>
+			<strong>Right to Erasure:</strong> You can request the deletion of your personal information.
+		</li>
+		<li>
+			<strong>Right to Object:</strong> You can object to the processing of your personal information.
+		</li>
+	</ul>
+
+	<p>
+		To exercise these rights, please contact us at<a href="mailto:tim@timsmith.tech"
+			>tim@timsmith.tech</a
+		>.
+	</p>
+
+	<h4>Cookie Policy</h4>
+	<p>
+		We use cookies to enhance your experience on our website. By using our website, you consent to
+		our use of cookies.
+	</p>
+
+	<h4>Changes to This Privacy Statement</h4>
+	<p>
+		We may update this privacy statement to reflect changes in our practices or for other
+		operational, legal, or regulatory reasons. Please check this page regularly for updates.
+	</p>
+
+	<h4>Contact Information</h4>
+	<p>
+		If you have any questions or concerns about our CPRA compliance or privacy practices, please
+		contact us at
+		<a href="mailto:tim@timsmith.tech">tim@timsmith.tech</a>. Last Updated: 02/03/2024
+	</p>
+</section>
+
+<style>
+	p {
+		margin-bottom: var(--padding-md);
+	}
+
+	ul {
+		margin-left: var(--padding-md);
+		list-style-type: disc;
+	}
+</style>
