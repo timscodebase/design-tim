d541f9a08cfac11214f2aefdd59a12be14cf1ee5
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-05-22 14:00:31 -0700

    chore: Add @types/gtag.js and "ai" npm dependencies

--- src/lib/components/Analytics/Analytics.svelte
+++ src/lib/components/Analytics/Analytics.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
-import { dev } from "$app/environment";
-import { inject } from "@vercel/analytics";
+	import { page } from '$app/stores'
+	const MEASUREMENT_ID = 'G-EPNRTTEP8M'
 
-inject({ mode: dev ? "development" : "production" });
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', MEASUREMENT_ID, {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			})
+		}
+	}
 </script>
+
+<svelte:head>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}>
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || []
+
+		function gtag() {
+			dataLayer.push(arguments)
+		}
+
+		gtag('js', new Date())
+		gtag('config', MEASUREMENT_ID)
+	</script>
+</svelte:head>
