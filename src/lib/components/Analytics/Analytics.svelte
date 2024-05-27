5e196838d2adbec99681ebb8b0ac3aaa9783c63e
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-05-26 17:32:49 -0700

    chore: Update Google Analytics configuration with new measurement ID

--- src/lib/components/Analytics/Analytics.svelte
+++ src/lib/components/Analytics/Analytics.svelte
@@ -19,14 +19,15 @@
 		partytown = {
 			forward: ['dataLayer.push', 'gtag']
 		}
-	</script><script async src="https://www.googletagmanager.com/gtag/js?id=G-EPNRTTEP8M"></script>
+	</script>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}>
+	</script>
 	<script>
 		window.dataLayer = window.dataLayer || []
 		function gtag() {
 			dataLayer.push(arguments)
 		}
 		gtag('js', new Date())
-
-		gtag('config', 'G-EPNRTTEP8M')
+		gtag('config', MEASUREMENT_ID)
 	</script>
 </svelte:head>
